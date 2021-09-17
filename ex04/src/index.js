c791892fb7f12b11abc06c9f758245ead085f826
Edin <edinosmic77@gmail.com>
2021-09-17 08:27:14 +0200

    adding ex04 to Repo04

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    // only change code below this line
+    myDecimal = 8.2;
+    myDecimalE = 0.00254;
+        
+    // only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
