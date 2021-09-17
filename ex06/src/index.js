8bf246461e9d1b7ca3a1f12d651539be2f830348
Edin <edinosmic77@gmail.com>
2021-09-17 08:43:00 +0200

    adding ex06 to Repo04

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo){
+
+    // only change code below this line
+    var myQuotient;
+    var numOne;
+    var numTwo;
+
+    myQuotient = numOne / numTwo;
+        
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0));
+console.log(main(16.5, 5.5));
+console.log(main(102.0, 25.5));
+module.exports = main;
